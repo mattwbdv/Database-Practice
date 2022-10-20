cfd2b827de4ba0f546bf88db1a2a1548bc75e3fe
matt <32993081+permutation-sort@users.noreply.github.com>
2022-10-20 14:28:16 -0700

    Update db.sql
    fixing patient

--- db.sql
+++ db.sql
@@ -61,25 +61,38 @@ CREATE TABLE Emergency_Contact (
     name VARCHAR(50), 
     relationship VARCHAR(25)
 );
+CREATE TABLE Patient_Admitted (
+	admit_id VARCHAR(10) PRIMARY KEY, 
+	room_id VARCHAR(10),
+    patient_id VARCHAR(10),
+	timestamp timestamp NOT NULL,
+	administrator_id VARCHAR(10), 
+    primary_doctor_id VARCHAR(10),
+	FOREIGN KEY (room_id) REFERENCES Room(room_id), 
+    FOREIGN KEY (patient_id) REFERENCES Patient(patient_id),
+    FOREIGN KEY (administrator_id) REFERENCES Administrator(administrator_id), 
+	FOREIGN KEY (primary_doctor_id) REFERENCES Primary_Doctor(primary_doctor_id)
+
+);
+CREATE TABLE Patient_Discharged (
+	discharge_id VARCHAR(10) PRIMARY KEY,
+    patient_id VARCHAR(10),
+	timestamp timestamp NOT NULL,
+    FOREIGN KEY (patient_id) REFERENCES Patient(patient_id)
+);
 CREATE TABLE Patient ( 
     patient_id VARCHAR(10) PRIMARY KEY, 
     insurance_id VARCHAR(10), 
     emergency_contact_id VARCHAR(10), 
-    administrator_id VARCHAR(10), 
-    room_id VARCHAR(10),
-    order_treatment_id VARCHAR(10),
+	order_treatment_id VARCHAR(10),
     administer_treatment_id VARCHAR(10),
-    primary_doctor_id VARCHAR(10),
     FOREIGN KEY (insurance_id) REFERENCES Insurance(insurance_id), 
     FOREIGN KEY (emergency_contact_id) REFERENCES Emergency_Contact(emergency_contact_id), 
-    FOREIGN KEY (administrator_id) REFERENCES Administrator(administrator_id), 
-	FOREIGN KEY (room_id) REFERENCES Room(room_id), 
 	FOREIGN KEY (order_treatment_id) REFERENCES OrderTreatment(order_treatment_id), 
-	FOREIGN KEY (administer_treatment_id) REFERENCES Administer_Treatment(administer_treatment_id), 
-	FOREIGN KEY (primary_doctor_id) REFERENCES Primary_Doctor(primary_doctor_id) );
+	FOREIGN KEY (administer_treatment_id) REFERENCES Administer_Treatment(administer_treatment_id)
+ );
 CREATE TABLE Room (
     room_id VARCHAR(10) PRIMARY KEY, 
     room_num int, 
     check (room_num between 1 and 20)
 );
-
