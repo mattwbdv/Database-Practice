65c75b32c20279a4bcb224b137c32c7ca9708683
matt <32993081+permutation-sort@users.noreply.github.com>
2022-10-20 15:30:56 -0700

    Update db.sql
    final updates

--- db.sql
+++ db.sql
@@ -27,27 +27,43 @@ CREATE TABLE Administrator (
 -- TREATMENT INFO
 CREATE TABLE OrderTreatment (
     order_treatment_id VARCHAR(10) PRIMARY KEY, 
-    is_medication bool,
 	timestamp timestamp NOT NULL,
-    primary_doctor_id VARCHAR(10) NOT NULL,
-    FOREIGN KEY (primary_doctor_id) REFERENCES Primary_Doctor(primary_doctor_id)
+    doctor_id VARCHAR(10) NOT NULL,
+    treatment_id VARCHAR(10),
+    admit_id VARCHAR(10),
+	FOREIGN KEY (admit_id) REFERENCES Patient_Admitted(admit_id),
+    FOREIGN KEY (doctor_id) REFERENCES Doctor(doctor_id), 
+	FOREIGN KEY (treatment_id) REFERENCES Treatment(treatment_id)
+);
+CREATE TABLE Treatment (
+	treatment_id VARCHAR(10),
+    is_procedure boolean
 );
 CREATE TABLE PerformTreatment (
     perform_treatment_id VARCHAR(10) PRIMARY KEY, 
-    timestamp timestamp NOT NULL,
-    employee_id VARCHAR(10) NOT NULL,
-    order_treatment_id VARCHAR(10) NOT NULL,
-    FOREIGN KEY (employee_id) REFERENCES Employee(employee_id),
-    FOREIGN KEY (order_treatment_id) REFERENCES OrderTreatment(order_treatment_id)
+	timestamp timestamp NOT NULL,
+    doctor_id VARCHAR(10) NOT NULL,
+    treatment_id VARCHAR(10),
+	admit_id VARCHAR(10),
+	FOREIGN KEY (admit_id) REFERENCES Patient_Admitted(admit_id),
+    FOREIGN KEY (doctor_id) REFERENCES Doctor(doctor_id), 
+	FOREIGN KEY (treatment_id) REFERENCES Treatment(treatment_id) 
 );
 CREATE TABLE Administer_Treatment (
     administer_treatment_id VARCHAR(10) PRIMARY KEY,
     timestamp timestamp NOT NULL,
-    employee_id VARCHAR(10) NOT NULL,
-    order_treatment_id VARCHAR(10) NOT NULL,
-    FOREIGN KEY (employee_id) REFERENCES Employee(employee_id),
-    FOREIGN KEY (order_treatment_id) REFERENCES OrderTreatment(order_treatment_id)
+	treatment_id VARCHAR(10),
+	admit_id VARCHAR(10),
+	FOREIGN KEY (admit_id) REFERENCES Patient_Admitted(admit_id),
+	FOREIGN KEY (treatment_id) REFERENCES Treatment(treatment_id) 
 );
+CREATE TABLE Employees_Involved (
+	administer_treatment_id VARCHAR(10),
+    employee_id VARCHAR(10) NOT NULL,
+    FOREIGN KEY (employee_id) REFERENCES Employee(employee_id),
+    FOREIGN KEY (administer_treatment_id) REFERENCES Administer_Treatment(adminster_treatment_id)
+);
+
 
 -- PATIENT INFO
 CREATE TABLE Insurance (
@@ -68,31 +84,43 @@ CREATE TABLE Patient_Admitted (
 	timestamp timestamp NOT NULL,
 	administrator_id VARCHAR(10), 
     primary_doctor_id VARCHAR(10),
+    diagnosis VARCHAR(50),
+	insurance_id VARCHAR(10), 
+    FOREIGN KEY (insurance_id) REFERENCES Insurance(insurance_id), 
 	FOREIGN KEY (room_id) REFERENCES Room(room_id), 
     FOREIGN KEY (patient_id) REFERENCES Patient(patient_id),
     FOREIGN KEY (administrator_id) REFERENCES Administrator(administrator_id), 
 	FOREIGN KEY (primary_doctor_id) REFERENCES Primary_Doctor(primary_doctor_id)
+);
 
+CREATE TABLE Assigned_To (
+	admit_id VARCHAR(10),
+	patient_id VARCHAR(10),
+	doctor_id VARCHAR(10),
+	is_primary bool,
+	FOREIGN KEY (admit_id) REFERENCES Patient_Admitted(admit_id), 
+	FOREIGN KEY (admit_id) REFERENCES Patient_Admitted(admit_id), 
+	FOREIGN KEY (doctor_id) REFERENCES Doctor(doctor_id)
 );
 CREATE TABLE Patient_Discharged (
 	discharge_id VARCHAR(10) PRIMARY KEY,
+    admit_id VARCHAR(10),
     patient_id VARCHAR(10),
 	timestamp timestamp NOT NULL,
-    FOREIGN KEY (patient_id) REFERENCES Patient(patient_id)
+    FOREIGN KEY (patient_id) REFERENCES Patient(patient_id), 
+	FOREIGN KEY (admit_id) REFERENCES Patient_Admitted(admit_id)
 );
 CREATE TABLE Patient ( 
     patient_id VARCHAR(10) PRIMARY KEY, 
-    insurance_id VARCHAR(10), 
     emergency_contact_id VARCHAR(10), 
 	order_treatment_id VARCHAR(10),
     administer_treatment_id VARCHAR(10),
-    FOREIGN KEY (insurance_id) REFERENCES Insurance(insurance_id), 
-    FOREIGN KEY (emergency_contact_id) REFERENCES Emergency_Contact(emergency_contact_id), 
-	FOREIGN KEY (order_treatment_id) REFERENCES OrderTreatment(order_treatment_id), 
-	FOREIGN KEY (administer_treatment_id) REFERENCES Administer_Treatment(administer_treatment_id)
+    FOREIGN KEY (emergency_contact_id) REFERENCES Emergency_Contact(emergency_contact_id) 
  );
 CREATE TABLE Room (
     room_id VARCHAR(10) PRIMARY KEY, 
     room_num int, 
-    check (room_num between 1 and 20)
+    check (room_num between 1 and 20), 
+    occupied bool
 );
+
